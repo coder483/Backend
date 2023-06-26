1f5f0022a2a709e5cc0e43ee190c688f02ea5f61
krishna <krishna60523@gmail.com>
2023-06-27 01:28:03 +0530

    add cloudinar properly

--- config/server.js
+++ config/server.js
@@ -5,6 +5,6 @@ module.exports = ({ env }) => ({
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', true),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
